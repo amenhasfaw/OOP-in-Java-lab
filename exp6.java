578709f13f8c5343568d1d8dc49e7130fcda9c47
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2022-12-04 18:25:08 +0530

    Exp N0 4

--- exp6.java
+++ exp6.java
@@ -1,35 +1,75 @@
-class T1 extends Thread {
-    public void run() {
-        try {
-            while (true) {
-                System.out.println("First");
-                Thread.sleep(2000);
-            }
-        } catch (InterruptedException e) {
-            System.out.println("Error");
-        }
+
+import java.util.*;
+
+class LessBalanceException extends Exception // Defining user-defined exception class “LessBalanceException”
+{
+
+    LessBalanceException(String s) // Constructor which takes user error message
+    {
+        super(s); // calls super class constructor
     }
 }
 
-class T2 extends Thread {
-    public void run() {
+class Account // class account
+{
+
+    double minbal;
+
+    Account() {
+        minbal = 1000; // minimum balance of any account is made 1000rs
+    }
+
+    void deposit(double amt) {
+        minbal = minbal + amt; // deposit amount is added to minimum balance
+    }
+
+    void withdraw(double amt) {
+        double t;
+        t = minbal;
+        t = t - amt;
         try {
-            while (true) {
-                System.out.println("Second");
-                Thread.sleep(4000);
+            if (t < 1000) // check against balance not to be lesser than 1000rs at any time
+                throw new LessBalanceException("Balance can't go less then 1000 (Withdraw amount is not valid)");
+            else {
+                minbal = minbal - amt; // withdraw amount is subtracted from latest minimum balance
+                System.out.print("Balance after withdraw:");
+                System.out.println(minbal);
             }
-        } catch (InterruptedException e) {
-            System.out.println("Error");
+        } catch (LessBalanceException l) {
+            System.out.println(l.getMessage());
         }
+    }
+
+    double balance() {
+        return minbal;
     }
 }
 
-class exp6 {
-    public static void main(String[] args) {
-        T1 first = new T1();
-        T2 second = new T2();
-
-        first.start();
-        second.start();
+public class exp6 {
+    public static void main(String args[]) {
+        Account a1 = new Account(); // creating account1
+        Account a2 = new Account(); // creating account2
+        double dep, wit;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Min bal of account 1: ");
+        System.out.println(a1.balance()); // account1 minimum balance
+        System.out.println("Enter the amount to deposit: ");
+        dep = sc.nextDouble();
+        a1.deposit(dep); // account1 deposit method call
+        System.out.print("Balance after deposit:");
+        System.out.println(a1.balance());
+        System.out.println("Enter the amount to Withdraw: ");
+        wit = sc.nextDouble();
+        a1.withdraw(wit); // account1 withdraw method call
+        System.out.print("\nMin bal of account 2: ");
+        System.out.println(a2.balance()); // account2 minimum balance
+        System.out.println("Enter the amount to deposit: ");
+        dep = sc.nextDouble();
+        a2.deposit(dep); // account2 deposit method call
+        System.out.print("Balance after deposit:");
+        System.out.println(a2.balance());
+        System.out.println("Enter the amount to Withdraw: ");
+        wit = sc.nextDouble();
+        a2.withdraw(wit); // account2 withdraw method call
     }
 }
