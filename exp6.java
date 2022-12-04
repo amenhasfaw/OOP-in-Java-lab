e1f923f7960e6e58506859ff766a794a85896caa
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2022-12-04 18:07:08 +0530

    Update

--- exp6.java
+++ exp6.java
@@ -1,29 +1,28 @@
 class T1 extends Thread {
-    public void run(){
-       try{
-           while(true){
-            System.out.println("First");
-            Thread.sleep(2000);
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("First");
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Error");
         }
-       } catch(InterruptedException e){
-           System.out.println("Error");
-       }
     }
 }
 
 class T2 extends Thread {
-    public void run(){
-       try{
-           while(true){
-            System.out.println("Second");
-            Thread.sleep(4000);
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("Second");
+                Thread.sleep(4000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Error");
         }
-       } catch(InterruptedException e){
-           System.out.println("Error");
-       }
     }
 }
-
 
 class exp6 {
     public static void main(String[] args) {
