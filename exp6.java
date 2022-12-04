542ce258eebaace03d123293f226fe974a7d7f2b
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2022-12-04 18:36:26 +0530

    Update

--- exp6.java
+++ exp6.java
@@ -50,26 +50,27 @@ public class exp6 {
         Account a1 = new Account(); // creating account1
         Account a2 = new Account(); // creating account2
         double dep, wit;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Min bal of account 1: ");
-        System.out.println(a1.balance()); // account1 minimum balance
-        System.out.println("Enter the amount to deposit: ");
-        dep = sc.nextDouble();
-        a1.deposit(dep); // account1 deposit method call
-        System.out.print("Balance after deposit:");
-        System.out.println(a1.balance());
-        System.out.println("Enter the amount to Withdraw: ");
-        wit = sc.nextDouble();
-        a1.withdraw(wit); // account1 withdraw method call
-        System.out.print("\nMin bal of account 2: ");
-        System.out.println(a2.balance()); // account2 minimum balance
-        System.out.println("Enter the amount to deposit: ");
-        dep = sc.nextDouble();
-        a2.deposit(dep); // account2 deposit method call
-        System.out.print("Balance after deposit:");
-        System.out.println(a2.balance());
-        System.out.println("Enter the amount to Withdraw: ");
-        wit = sc.nextDouble();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Min bal of account 1: ");
+            System.out.println(a1.balance()); // account1 minimum balance
+            System.out.println("Enter the amount to deposit: ");
+            dep = sc.nextDouble();
+            a1.deposit(dep); // account1 deposit method call
+            System.out.print("Balance after deposit:");
+            System.out.println(a1.balance());
+            System.out.println("Enter the amount to Withdraw: ");
+            wit = sc.nextDouble();
+            a1.withdraw(wit); // account1 withdraw method call
+            System.out.print("\nMin bal of account 2: ");
+            System.out.println(a2.balance()); // account2 minimum balance
+            System.out.println("Enter the amount to deposit: ");
+            dep = sc.nextDouble();
+            a2.deposit(dep); // account2 deposit method call
+            System.out.print("Balance after deposit:");
+            System.out.println(a2.balance());
+            System.out.println("Enter the amount to Withdraw: ");
+            wit = sc.nextDouble();
+        }
         a2.withdraw(wit); // account2 withdraw method call
     }
 }
