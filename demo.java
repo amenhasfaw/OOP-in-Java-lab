f805898b4f00c255788a4a769f3418c3c715f51d
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2022-12-06 11:10:54 +0530

    Method overloading

---
+++ demo.java
@@ -0,0 +1,33 @@
+// METHOD OVERLOAD
+
+// class overload{
+//     int a = 5;
+//     int b = 6;
+
+//     void add(){
+//         int ans = a+b;
+//         System.out.println("First method: a + b = " + ans);
+//     }
+
+//     void add(int x,int y){
+//         a=x;
+//         b=y;
+//         int ans = a+b;
+//         System.out.println("Second method: a + b = " + ans);
+
+//     }
+// }
+
+// public class demo{
+//     public static void main(String args[]){
+//         overload m1 = new overload();
+//         m1.add();
+//         m1.add(4,5);
+//     }
+// }
+
+
+
+
+
+
